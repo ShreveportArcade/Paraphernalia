10f675ae38257df271b30922514756a5fb3b04ad
Nolan Baker <nolan.lee.baker@gmail.com>
2014-05-01 17:15:24 -0500

    adds rigidbody 2d extensions

---
+++ Extensions/Rigidbody2DExtensions.cs
@@ -0,0 +1,27 @@
+﻿// From: http://forum.unity3d.com/threads/213397-Rigidbody2D-ForceMode-Impulse
+//       http://answers.unity3d.com/questions/574864/rigidbody2d-and-forcemodeaddvelocity.html
+// Author: gfoot 
+// made into an extension by Kencho
+
+using UnityEngine;
+using System.Collections;
+
+public static class Rigidbody2DExtensions {
+
+	public static void AddForce(this Rigidbody2D rigidbody2D, Vector2 force, ForceMode mode = ForceMode.Force) {
+		switch (mode) {
+		case ForceMode.Force:
+			rigidbody2D.AddForce(force);
+			break;
+		case ForceMode.Impulse:
+			rigidbody2D.AddForce(force / Time.fixedDeltaTime);
+			break;
+		case ForceMode.Acceleration:
+			rigidbody2D.AddForce(force * rigidbody2D.mass);
+			break;
+		case ForceMode.VelocityChange:
+			rigidbody2D.AddForce(force * rigidbody2D.mass / Time.fixedDeltaTime);
+			break;
+		}
+	}
+}
