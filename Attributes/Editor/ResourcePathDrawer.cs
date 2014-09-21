1e3547336b32b862d3c9d9eee5d6e1b3acfa7382
Nolan Baker <nolan.lee.baker@gmail.com>
2014-09-20 19:29:53 -0500

    resource path now correct, no longer asset path

--- Attributes/Editor/ResourcePathDrawer.cs
+++ Attributes/Editor/ResourcePathDrawer.cs
@@ -29,11 +29,16 @@ public class ResourcePathDrawer : PropertyDrawer {
 			Type objType = ((ResourcePathAttribute)attribute).resourceType;
 			UnityEngine.Object obj = null;
 			if (property.stringValue != "") {
-				obj = AssetDatabase.LoadAssetAtPath(property.stringValue, objType);
+				obj = Resources.Load(property.stringValue, objType);
 			}
 
 			obj = EditorGUI.ObjectField(rect, label, obj, objType, false);
-			property.stringValue = AssetDatabase.GetAssetPath(obj);
+			string assetPath = AssetDatabase.GetAssetPath(obj);
+			if (assetPath != "") {
+				string[] split = assetPath.Split(new string[]{"Resources/"}, StringSplitOptions.RemoveEmptyEntries);
+				string ext = System.IO.Path.GetExtension(assetPath);
+				property.stringValue = split[split.Length-1].Replace(ext, "");
+			}
 		}
 	}
 }
