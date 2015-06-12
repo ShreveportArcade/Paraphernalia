5a4a3efc78c20e636c375136cec4bea73bf42e1a
Nolan Baker <nolan.lee.baker@gmail.com>
2015-06-12 17:35:14 -0500

    AudioManager can now cross fade music

--- Components/AudioManager.cs
+++ Components/AudioManager.cs
@@ -1,11 +1,34 @@
-﻿using UnityEngine;
-using System.Collections;
+﻿/*
+Copyright (C) 2015 Nolan Baker
 
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated 
+documentation files (the "Software"), to deal in the Software without restriction, including without limitation 
+the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, 
+and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions 
+of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED 
+TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL 
+THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF 
+CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER 
+DEALINGS IN THE SOFTWARE.
+*/
+
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Paraphernalia.Components {
 public class AudioManager : MonoBehaviour {
 
 	[Range(0,10)] public int sfxSourcesCount = 5;
 	private int currentSFXSource = 0;
 	private AudioSource[] sfxSources;
+
+	private int currentMusicSource = 0;
+	private AudioSource[] musicSources;
 
 	private static AudioManager _instance;
 	public static AudioManager instance {
@@ -25,7 +48,8 @@ public class AudioManager : MonoBehaviour {
 		if (_instance == null) {
 			_instance = this;
 			DontDestroyOnLoad(_instance.gameObject);
-			CreateFXPool();
+			CreateSFXPool();
+			CreateMusicPool();
 		}
 		else if (_instance != this) {
 			Debug.LogWarning("AudioManager already initialized, destroying duplicate");
@@ -33,7 +57,7 @@ public class AudioManager : MonoBehaviour {
 		}
 	}
 
-	void CreateFXPool () {
+	void CreateSFXPool () {
 		sfxSources = new AudioSource[sfxSourcesCount];
 		for (int i = 0; i < sfxSourcesCount; i++) {
 			GameObject sfxSource = new GameObject("SFXSource." + i);
@@ -42,9 +66,51 @@ public class AudioManager : MonoBehaviour {
 		}
 	}
 
+	void CreateMusicPool () {
+		musicSources = new AudioSource[2];
+		for (int i = 0; i < 2; i++) {
+			GameObject musicSource = new GameObject("MusicSource." + i);
+			musicSource.transform.parent = transform;
+			musicSources[i] = musicSource.AddComponent<AudioSource>();
+			musicSources[i].loop = true;
+		}
+	}
+
 	public static void PlayEffect(AudioClip clip) {
 		AudioSource source = instance.sfxSources[instance.currentSFXSource];
 		source.PlayOneShot(clip, 1);
 		instance.currentSFXSource = (instance.currentSFXSource + 1) % instance.sfxSourcesCount;
 	}
+
+	public static void PlayMusic(AudioClip clip) {
+		AudioSource currentSource = instance.musicSources[instance.currentMusicSource];
+		currentSource.clip = clip;
+		currentSource.Play();
+	}
+
+	public static void CrossfadeMusic(AudioClip clip, float fadeDuration) {
+		instance.musicSources[(instance.currentMusicSource + 1) % 2].clip = clip;
+		instance.StartCoroutine("CrossfadeMusicCoroutine", fadeDuration);
+	}
+
+	IEnumerator CrossfadeMusicCoroutine(float fadeDuration) {
+		AudioSource sourceA = musicSources[currentMusicSource];
+		currentMusicSource = (currentMusicSource + 1) % 2;
+		AudioSource sourceB = musicSources[currentMusicSource];
+		sourceB.Play();
+
+		float t = 0;
+		while (t < fadeDuration) {
+			float frac = t / fadeDuration;
+			sourceA.volume = 1 - frac;
+			sourceB.volume = frac;
+			t += Time.deltaTime;
+			yield return new WaitForEndOfFrame();
+		}
+		
+		sourceA.volume = 0;
+		sourceA.Stop();
+		sourceB.volume = 1;
+	}
+}
 }
