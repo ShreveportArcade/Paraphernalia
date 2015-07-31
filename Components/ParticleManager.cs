5db468927cef0619cae1626fdc79322e9982beac
Nolan Baker <nolan.lee.baker@gmail.com>
2015-07-31 15:38:41 -0500

    adds particle manager and projectile

---
+++ Components/ParticleManager.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using Paraphernalia.Extensions;
+using Paraphernalia.Utils;
+
+public class ParticleManager : MonoBehaviour {
+
+	public ParticleSystem[] particlePrefabs;
+	[Range(0,10)] public int poolSize = 5;
+	public static ParticleManager instance;
+
+	private Dictionary<string, ParticleSystem[]> pools = new Dictionary<string, ParticleSystem[]>();
+	private Dictionary<string, int> currentIndices = new Dictionary<string, int>();
+
+	void Awake () {
+		if (instance != null) {
+			GameObjectUtils.Destroy(gameObject);
+		}
+		else {
+			instance = this;
+			CreateParticleSystemPool();
+		}
+	}
+
+	void CreateParticleSystemPool () {
+		for (int prefabIndex = 0; prefabIndex < particlePrefabs.Length; prefabIndex++) {
+			string name = particlePrefabs[prefabIndex].name;
+			pools[name] = new ParticleSystem[poolSize];
+			currentIndices[name] = 0;
+			for (int poolIndex = 0; poolIndex < poolSize; poolIndex++) {
+				ParticleSystem particleSystem = particlePrefabs[prefabIndex].Instantiate() as ParticleSystem;
+				particleSystem.transform.parent = transform;
+				pools[name][poolIndex] = particleSystem;
+			}
+		}
+	}
+
+	public static void Play(string name, Vector3 position, Color? color = null) {
+		int index = instance.currentIndices[name];
+		ParticleSystem particleSystem = instance.pools[name][index];
+		particleSystem.transform.position = position;
+		if (color != null) particleSystem.startColor = color.Value;
+		particleSystem.Play();
+		index = (index + 1) % instance.poolSize;
+		instance.currentIndices[name] = index;
+	}
+}
---
+++ Components/Projectile.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+using Paraphernalia.Extensions;
+using Paraphernalia.Utils;
+using Paraphernalia.Components;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class Projectile : MonoBehaviour {
+
+	public float speed = 5;
+	public ParticleSystem particles;
+	public AudioClip explosionSound;
+	public string onHitParticleSystemName = "PlasmaExplosion";
+
+	public void Fire (Vector3 position, Vector3 direction) {
+		gameObject.SetActive(true);
+		transform.position = position;
+		GetComponent<Rigidbody2D>().velocity = direction.normalized * speed;
+		particles.Play();
+	}
+
+	void OnTriggerEnter2D (Collider2D collider) {
+		OnHit();
+	}
+
+	void OnCollisionEnter2D (Collision2D collision) {
+		OnHit();
+	}
+
+	void OnHit() {
+		AudioManager.PlayEffect(explosionSound);
+		ParticleManager.Play(onHitParticleSystemName, transform.position);
+		gameObject.SetActive(false);
+		CameraShake.MainCameraShake();
+	}
+}
