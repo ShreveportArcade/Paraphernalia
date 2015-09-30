fbf263b4d07c1a2cb7a1b74643ca6f821506b64d
Nolan Baker <nolan.lee.baker@gmail.com>
2015-09-30 01:47:48 -0500

    fixes off-by-one error in Polygon.GetPathOffset, ParticleManager can scale particle fx, adds scale/strength to Projectile

--- Components/ParticleManager.cs
+++ Components/ParticleManager.cs
@@ -10,6 +10,7 @@ public class ParticleManager : MonoBehaviour {
 	[Range(0,10)] public int poolSize = 5;
 	public static ParticleManager instance;
 
+	private Dictionary<string, ParticleSystem> prefabs = new Dictionary<string, ParticleSystem>();
 	private Dictionary<string, ParticleSystem[]> pools = new Dictionary<string, ParticleSystem[]>();
 	private Dictionary<string, int> currentIndices = new Dictionary<string, int>();
 
@@ -28,6 +29,7 @@ public class ParticleManager : MonoBehaviour {
 			string name = particlePrefabs[prefabIndex].name;
 			pools[name] = new ParticleSystem[poolSize];
 			currentIndices[name] = 0;
+			prefabs[name] = particlePrefabs[prefabIndex];
 			for (int poolIndex = 0; poolIndex < poolSize; poolIndex++) {
 				ParticleSystem particleSystem = particlePrefabs[prefabIndex].Instantiate() as ParticleSystem;
 				particleSystem.transform.parent = transform;
@@ -41,14 +43,41 @@ public class ParticleManager : MonoBehaviour {
 	}
 
 	public static void Play(string name, Vector3 position, Vector3 normal, Color? color = null) {
+		Play(name, position, normal, 1, color);
+	}
+
+	public static void Play(string name, Vector3 position, Vector3 normal, float size, Color? color = null) {
 		if (instance == null || !instance.currentIndices.ContainsKey(name)) return;
 		int index = instance.currentIndices[name];
 		ParticleSystem particleSystem = instance.pools[name][index];
 		particleSystem.transform.position = position;
 		particleSystem.transform.up = normal;
+		// particleSystem.transform.localScale = Vector3.one * size; // This didn't work.
+		ParticleSystem prefab = instance.prefabs[name]; // THIS IS PROBABLY DUMB AND EXPENSIVE
+		ScaleParticleSystem(particleSystem, prefab, size);
 		if (color != null) particleSystem.startColor = color.Value;
 		particleSystem.Play();
 		index = (index + 1) % instance.poolSize;
 		instance.currentIndices[name] = index;
+	}
+
+	public static void ScaleParticleSystem(ParticleSystem s, ParticleSystem p, float size) {
+		s.startSize = p.startSize * size;
+		s.startSpeed = p.startSpeed * size;
+		for (int i = 0; i < p.transform.childCount; i++) {
+            GameObject pChild = p.transform.GetChild(i).gameObject;
+            ParticleSystem pChildSys = pChild.GetComponent<ParticleSystem>();
+            if (pChildSys == null) continue;
+
+            GameObject sChild = s.transform.GetChild(i).gameObject;
+			ParticleSystem sChildSys = sChild.GetComponent<ParticleSystem>();
+            if (sChildSys == null) {
+            	Debug.LogWarning("ParticleManager: Instance of prefab particle system is no longer like parent.");
+            	continue;
+            }
+            sChildSys.startSize = pChildSys.startSize * size;
+            sChildSys.startSpeed = pChildSys.startSpeed * size;
+        }
+		
 	}
 }
--- Components/Projectile.cs
+++ Components/Projectile.cs
@@ -9,13 +9,22 @@ public class Projectile : MonoBehaviour {
 
 	public float speed = 5;
 	public float lifetime = 3;
+	public float size = 1;
+	[Range(0,1)] public float pursuitDamping = 0.1f;
 	[Range(0,1)] public float gunVelocityDamping = 0.1f;
 	public ParticleSystem particles;
 	public string onHitAudioClipName = "laserHit";
 	public bool shakeCamera = true;
 	public string onHitParticleSystemName = "PlasmaExplosion";
 	public Color onHitColor = Color.white;
+	public Rigidbody2D target;
 	
+	new private Rigidbody2D rigidbody2D;
+
+	void Awake () {
+		rigidbody2D = GetComponent<Rigidbody2D>();
+	}
+
 	public void Fire (Vector3 position, Vector3 direction, Vector3 gunVelocity = default(Vector3)) {
 		transform.right = direction;
 		gameObject.SetActive(true);
@@ -45,8 +54,16 @@ public class Projectile : MonoBehaviour {
 	public void OnHit(Vector3 normal) {
 		StopCoroutine("LifeCycleCoroutine");
 		AudioManager.PlayEffect(onHitAudioClipName, transform, Random.Range(0.7f, 1), Random.Range(0.95f, 1.05f));
-		ParticleManager.Play(onHitParticleSystemName, transform.position, normal, onHitColor);
+		ParticleManager.Play(onHitParticleSystemName, transform.position, normal, size * size, onHitColor);
 		gameObject.SetActive(false);
 		if (shakeCamera) CameraShake.MainCameraShake();
+	}
+
+	void Update () {
+		if (target != null && speed > 0) {
+			Vector2 steering = Steering.Seek(rigidbody2D, target.transform.position, speed);
+			rigidbody2D.AddForce(steering * pursuitDamping, ForceMode.VelocityChange);
+			transform.right = rigidbody2D.velocity.normalized;
+		}
 	}
 }
