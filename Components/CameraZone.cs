81e7816bd13bd0376e8dc09b84fe70b365f34621
Nolan Baker <nolan.lee.baker@gmail.com>
2015-11-07 23:41:17 -0600

    adds behaviours to activate to camera zones

--- Components/CameraZone.cs
+++ Components/CameraZone.cs
@@ -7,6 +7,8 @@ public class CameraZone : MonoBehaviour {
 	public AudioClip music;
 	public Vector3 offset = new Vector3(0,0,-150);
 	public float transitionTime = 1;
+	public MonoBehaviour[] behavioursToActivate;
+
 	public Vector3 position {
 		get {
 			return transform.position + offset;
@@ -16,12 +18,18 @@ public class CameraZone : MonoBehaviour {
 	void OnTriggerEnter2D(Collider2D collider) {
 		if (collider.gameObject.tag == "Player") {
 			CameraController.AddZone(this);
+			foreach (MonoBehaviour b in behavioursToActivate) {
+				b.enabled = true;
+			}
 		}
 	}
 
 	void OnTriggerExit2D(Collider2D collider) {
 		if (collider.gameObject.tag == "Player") {
 			CameraController.RemoveZone(this);
+			foreach (MonoBehaviour b in behavioursToActivate) {
+				b.enabled = false;
+			}
 		}
 	}
 
