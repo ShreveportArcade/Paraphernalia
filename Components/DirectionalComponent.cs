205f43a56f1f4177b305e53b85853b4b81b4b7ed
Nolan Baker <nolan.lee.baker@gmail.com>
2016-04-02 00:44:15 -0500

    adds directional components

---
+++ Components/DirectionalComponent.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public abstract class DirectionalComponent : MonoBehaviour {
+
+	public void SetDirection (Vector2 dir) {
+		if (Mathf.Abs(dir.x) > Mathf.Abs(dir.y)) {
+			if (dir.x < 0) SetLeft();
+			else SetRight();
+		}
+		else {
+			if (dir.y < 0) SetDown();
+			else SetUp();
+		}
+	}
+
+	protected abstract void SetLeft();
+	protected abstract void SetRight();
+	protected abstract void SetUp();
+	protected abstract void SetDown();	
+}
---
+++ Components/DirectionalSprite.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(SpriteRenderer))]
+public class DirectionalSprite : DirectionalComponent {
+
+	public Sprite leftSprite;
+	public Sprite rightSprite;
+	public Sprite upSprite;
+	public Sprite downSprite;
+
+	private SpriteRenderer _spriteRenderer;
+	public SpriteRenderer spriteRenderer {
+		get {
+			if (_spriteRenderer == null) {
+				_spriteRenderer = GetComponent<SpriteRenderer>();	
+			}
+			return _spriteRenderer;
+		}
+	}
+
+	[ContextMenu("Look Left")]
+	protected override void SetLeft () {
+		spriteRenderer.sprite = leftSprite;
+	}
+
+	[ContextMenu("Look Right")]
+	protected override void SetRight () {
+		spriteRenderer.sprite = rightSprite;
+	}
+
+	[ContextMenu("Look Up")]
+	protected override void SetUp () {
+		spriteRenderer.sprite = upSprite;
+	}
+
+	[ContextMenu("Look Down")]
+	protected override void SetDown () {
+		spriteRenderer.sprite = downSprite;
+	}
+}
