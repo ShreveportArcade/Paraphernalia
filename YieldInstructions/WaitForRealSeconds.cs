47825c9d0f525ffafc80344d23bf618ceed3763d
Nolan Baker <nolan.lee.baker@gmail.com>
2016-04-21 01:54:22 -0500

    adds WaitForRealSeconds to YieldInstrucitons

---
+++ YieldInstructions/WaitForRealSeconds.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Paraphernalia.YieldInstructions {
+public class WaitForRealSeconds : CustomYieldInstruction {
+
+	private float startTime;
+	private float duration;
+
+	public override bool keepWaiting { 
+		get { return (Time.realtimeSinceStartup - this.startTime) < this.duration; } 
+	}
+
+	public WaitForRealSeconds (float seconds) { 
+		this.startTime = Time.realtimeSinceStartup;
+		this.duration = seconds;
+	}
+}
+}
