e992fed45a5dd407b2d08c3af6b15d7aaedad1c0
MacMasterBlaster <mac.orchard@yahoo.com>
2016-04-27 20:14:53 -0500

    damage optionally affects ancestors

--- Components/Damage.cs
+++ Components/Damage.cs
@@ -7,6 +7,7 @@ public class Damage : MonoBehaviour {
 	public float damage = 1;
 	public float multiplier = 1;
 	public bool disableOnCollision = false;
+    public bool affectAncestor = false;
 
 	protected virtual float GetDamage() {
 		if (disableOnCollision) gameObject.SetActive(false);
@@ -19,13 +20,13 @@ public class Damage : MonoBehaviour {
 
 	void OnTriggerEnter2D (Collider2D collider) {
 		HealthController h = collider.gameObject.GetComponent<HealthController>();
-		if (h == null) h = collider.gameObject.GetAncestorComponent<HealthController>();
-		if (h != null) h.TakeDamage(GetDamage());
+        if (affectAncestor && h == null) h = collider.gameObject.GetAncestorComponent<HealthController>();
+        if (h != null) h.TakeDamage(GetDamage());       
 	}
 
 	void OnCollisionEnter2D (Collision2D collision) {
 		HealthController h = collision.gameObject.GetComponent<HealthController>();
-		if (h == null) h = collision.gameObject.GetAncestorComponent<HealthController>();
-		if (h != null) h.TakeDamage(GetDamage(collision.relativeVelocity, collision.contacts[0].normal));
+        if (affectAncestor && h == null) h = collision.gameObject.GetAncestorComponent<HealthController>();
+        if (h != null) h.TakeDamage(GetDamage(collision.relativeVelocity, collision.contacts[0].normal));
 	}		
 }
