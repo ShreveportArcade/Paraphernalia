b57777b635d50cfb8d4abc8527eb49b7f99a76f6
Nolan Baker <nolan.lee.baker@gmail.com>
2016-05-02 21:38:32 -0500

    adds affect ancestor boolean to damage components

--- Components/ContinuousDamage.cs
+++ Components/ContinuousDamage.cs
@@ -5,16 +5,17 @@ using Paraphernalia.Extensions;
 public class ContinuousDamage : MonoBehaviour {
 
 	public float damagePerSecond = 1;
+	public bool affectAncestor = false;
 
 	void OnTriggerStay2D (Collider2D collider) {
 		HealthController h = collider.gameObject.GetComponent<HealthController>();
-		if (h == null) h = collider.gameObject.GetAncestorComponent<HealthController>();
+		if (h == null && affectAncestor) h = collider.gameObject.GetAncestorComponent<HealthController>();
 		if (h != null) h.TakeDamage(damagePerSecond * Time.deltaTime, false);
 	}
 
 	void OnCollisionStay2D (Collision2D collision) {
 		HealthController h = collision.gameObject.GetComponent<HealthController>();
-		if (h == null) h = collision.gameObject.GetAncestorComponent<HealthController>();
+		if (h == null && affectAncestor) h = collision.gameObject.GetAncestorComponent<HealthController>();
 		if (h != null) h.TakeDamage(damagePerSecond * Time.deltaTime, false);
 	}
 }
