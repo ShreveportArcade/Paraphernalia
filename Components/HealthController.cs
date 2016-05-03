6bd841ace8b7d50df52ebb513c42a5985c6d429b
MacMasterBlaster <mac.orchard@yahoo.com>
2016-05-03 18:40:31 -0500

    on death now called before disabling

--- Components/HealthController.cs
+++ Components/HealthController.cs
@@ -41,15 +41,15 @@ public class HealthController : MonoBehaviour {
 			}
 
 			if (_health <= 0 && prevHealth > 0) {
-				if (_health <= destructionHealth && prevHealth > destructionHealth) {
+                onAnyDeath(this);
+                onDeath();
+                if (_health <= destructionHealth && prevHealth > destructionHealth) {
 					PlayDestruction();
 				}
 				else {
 					AudioManager.PlayVariedEffect(deathSoundName);
 					ParticleManager.Play(deathParticlesName, transform);
 				}
-				onAnyDeath(this);
-				onDeath();
 			}
 			else if (_health <= destructionHealth && prevHealth > destructionHealth) {
 				PlayDestruction();
