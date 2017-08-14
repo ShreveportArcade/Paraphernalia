0366ed0c385168c6bee0f3b6f78622714087ba64
Nolan Baker <nolan.lee.baker@gmail.com>
2017-08-13 22:03:23 -0500

    fixes textWidth check

--- Extensions/UIExtensions.cs
+++ Extensions/UIExtensions.cs
@@ -7,9 +7,12 @@ public static class UIExtensions {
 
 	public static float TextWidth(this Text text) {
         float width = 0;
+        int fontSize = text.fontSize;
+        if (text.resizeTextForBestFit) fontSize = text.cachedTextGeneratorForLayout.fontSizeUsedForBestFit;
+        text.font.RequestCharactersInTexture(text.text, fontSize);
         CharacterInfo charInfo;
         foreach (char c in text.text) {
-            text.font.GetCharacterInfo(c, out charInfo, text.cachedTextGenerator.fontSizeUsedForBestFit);
+            text.font.GetCharacterInfo(c, out charInfo, fontSize);
             width += charInfo.advance; 
         }
         return width;
