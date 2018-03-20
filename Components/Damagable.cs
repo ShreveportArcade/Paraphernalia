07a44757ea650c3a0e11dee962b17ab3785d280d
Nolan Baker <nolan.lee.baker@gmail.com>
2018-03-20 10:55:50 -0500

    readds damagable

---
+++ Components/Damagable.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+[RequireComponent(typeof(SpriteRenderer))]
+public class Damagable : MonoBehaviour {
+
+	public HealthController healthController;
+	public bool flipRandomly = true;
+
+	[System.Serializable]
+	public class DamageState {
+		public float health;
+		public Sprite sprite;
+
+		public DamageState () {
+			health = 1;
+			sprite = null;
+		}
+	}
+
+	public List<DamageState> damageStates = new List<DamageState>();
+
+	private SpriteRenderer _spriteRenderer;
+	private SpriteRenderer spriteRenderer {
+		get {
+			if (_spriteRenderer == null) {
+				_spriteRenderer = GetComponent<SpriteRenderer>();
+			}
+			return _spriteRenderer;
+		}
+	}
+
+	void OnEnable () {
+		if (flipRandomly && spriteRenderer != null) spriteRenderer.flipX = (Random.value < 0.5f);
+		healthController.onHealthChanged += OnHealthChanged;
+		damageStates.Sort((d1, d2) => d1.health.CompareTo(d2.health));
+	}
+
+	void OnDisable () {
+		healthController.onHealthChanged -= OnHealthChanged;
+	}
+
+	void OnHealthChanged(float health, float prevHealth, float maxHealth) {
+		if (spriteRenderer == null) return;
+		foreach (DamageState state in damageStates) {
+			if (health < state.health) {
+				spriteRenderer.sprite = state.sprite;
+				break;
+			}
+		}
+	}
+}
