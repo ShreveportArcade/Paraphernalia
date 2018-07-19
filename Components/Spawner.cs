1c60d2613bbfc9ee645d19984cc1c3c7ebfe01bb
Nolan Baker <nolan.lee.baker@gmail.com>
2018-07-19 11:21:23 -0500

    updates spawner to allow per-scene pools

--- Components/Spawner.cs
+++ Components/Spawner.cs
@@ -5,91 +5,94 @@ using Paraphernalia.Extensions;
 
 public class Spawner : MonoBehaviour {
 
-	public static Spawner instance;
-	public static Transform root {
-		get {
-			if (instance == null) return null;
-			return instance.gameObject.transform;
-		}
-	}
+    public static Spawner instance;
+    public static List<Spawner> instances = new List<Spawner>();
+    public static Transform root {
+        get {
+            if (instance == null) return null;
+            return instance.gameObject.transform;
+        }
+    }
 
-	public bool reparentObjects = true;
-	public GameObject[] prefabs;
+    public bool reparentObjects = true;
+    public GameObject[] prefabs;
 
-	private Dictionary<string, GameObject> prefabsDict;
-	private Dictionary<string, List<GameObject>> poolsDict;
+    private Dictionary<string, GameObject> prefabsDict;
+    private Dictionary<string, List<GameObject>> poolsDict;
 
-	void Awake () {
-		if (instance == null) {
-			instance = this;
-			prefabsDict = new Dictionary<string, GameObject>();
-			poolsDict = new Dictionary<string, List<GameObject>>();
+    void Awake () {
+        if (instance == null) instance = this;
+        instances.Add(this);
 
-			foreach (GameObject prefab in prefabs) {
-				poolsDict[prefab.name] = new List<GameObject>();
-				prefabsDict[prefab.name] = prefab;
-			}
-		}
-	}
+        prefabsDict = new Dictionary<string, GameObject>();
+        poolsDict = new Dictionary<string, List<GameObject>>();
 
-	public static GameObject Prefab(string name) {
-		if (instance == null || 
-			string.IsNullOrEmpty(name) ||
-			!instance.prefabsDict.ContainsKey(name)) {
-			
-			return null;
-		}
+        foreach (GameObject prefab in prefabs) {
+            poolsDict[prefab.name] = new List<GameObject>();
+            prefabsDict[prefab.name] = prefab;
+        }
+    }
 
-		return instance.prefabsDict[name];
-	}
+    public static GameObject Prefab (string name) {
+        if (instance == null || string.IsNullOrEmpty(name)) return null;
 
-	public static GameObject Spawn(string name, bool active = true) {
-		if (instance == null || 
-			string.IsNullOrEmpty(name) ||
-			!instance.poolsDict.ContainsKey(name) ||
-			!instance.prefabsDict.ContainsKey(name)) {
-			
-			return null;
-		}
+        foreach (Spawner spawner in instances) {
+            if (!spawner.prefabsDict.ContainsKey(name)) continue;
+            return spawner.prefabsDict[name];
+        }
+        return null;
+    }
 
-		List<GameObject> pool = instance.poolsDict[name];
-		pool.RemoveAll((i) => i == null);
-		GameObject g = pool.Find((i) => !i.activeSelf);
-		if (g == null) {
-			g = instance.prefabsDict[name].Instantiate() as GameObject;
-			if (instance.reparentObjects) g.transform.SetParent(instance.transform);
-			pool.Add(g);
-		}
-		
-		g.SetActive(active);
-		
-		return g;
-	}
+    public static GameObject Spawn (string name, bool active = true) {
+        if (instance == null || string.IsNullOrEmpty(name)) return null;
 
-	public static void DisableAll() {
-		foreach (List<GameObject> pool in instance.poolsDict.Values) {
-			for (int i = pool.Count-1; i >= 0; i--) {
-				GameObject g = pool[i];
-				if (g == null) pool.RemoveAt(i);
-				else g.SetActive(false);
-			}
-		}
-	}
+        Spawner spawner = null;
+        foreach (Spawner s in instances) {
+            if (s.prefabsDict.ContainsKey(name)) {
+                spawner = s;
+                break;
+            }
+        }
 
-	public static void DisableAll(string name) {
-		if (instance == null || 
-			string.IsNullOrEmpty(name) ||
-			!instance.poolsDict.ContainsKey(name) ||
-			!instance.prefabsDict.ContainsKey(name)) {
-			
-			return;
-		}
+        if (spawner == null) return null;
+        List<GameObject> pool = spawner.poolsDict[name];
+        pool.RemoveAll((i) => i == null);
 
-		List<GameObject> pool = instance.poolsDict[name];
-		for (int i = pool.Count-1; i >= 0; i--) {
-			GameObject g = pool[i];
-			if (g == null) pool.RemoveAt(i);
-			else g.SetActive(false);
-		}
-	}
+        GameObject g = pool.Find((i) => !i.activeSelf);
+        if (g == null) {
+            g = Prefab(name).Instantiate() as GameObject;
+            if (spawner.reparentObjects) g.transform.SetParent(spawner.transform);
+            pool.Add(g);
+        }
+        g.SetActive(active);
+
+        return g;
+    }
+
+    public static void DisableAll() {
+        foreach (Spawner spawner in instances) {
+            foreach (List<GameObject> pool in spawner.poolsDict.Values) {
+                for (int i = pool.Count-1; i >= 0; i--) {
+                    GameObject g = pool[i];
+                    if (g == null) pool.RemoveAt(i);
+                    else g.SetActive(false);
+                }
+            }
+        }
+    }
+
+    public static void DisableAll(string name) {
+        if (instance == null || string.IsNullOrEmpty(name)) return;
+
+        foreach (Spawner spawner in instances) {
+            if (!spawner.poolsDict.ContainsKey(name)) continue;
+            List<GameObject> pool = spawner.poolsDict[name];
+            for (int i = pool.Count-1; i >= 0; i--) {
+                GameObject g = pool[i];
+                if (g == null) pool.RemoveAt(i);
+                else g.SetActive(false);
+            }
+            break;
+        }
+    }
 }
