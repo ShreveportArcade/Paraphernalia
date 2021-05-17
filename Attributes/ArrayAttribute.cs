06caffb44ae32417cf02ed329391496f0c3bfb15
Nolan Baker <nolan.lee.baker@gmail.com>
2021-05-17 10:22:38 -0500

    removes shaders, array attr, quick search, triangulator, follower, damagable, and follower

--- Attributes/ArrayAttribute.cs
+++
@@ -1,9 +0,0 @@
-﻿// from http://forum.unity3d.com/threads/240895-Generic-Editor-Array-PropertyAttribute-Tools
-// Author: cfloutier 
-
-using UnityEngine;
-using System.Collections;
-
-public class ArrayAttribute : PropertyAttribute {
-
-}
